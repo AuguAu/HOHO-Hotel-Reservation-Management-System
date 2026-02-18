b596013d7eb960ffb4b674849cd1a025eb94497b
AuguAu <august.hello11@gmail.com>
2026-02-19 00:52:42 +0700

    v1.1.0 Updated src/models/User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
- 
+
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Password
-  role: { type: String, enum: ['SuperAdmin', 'Admin'], default: 'Admin' }, // Role Base Access
-  profileImage: { type: String, default: '' } // Profile picture
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Manager', 'Receptionist'], default: 'Receptionist' },
+  name: { type: String, required: true }
 }, { timestamps: true });
- 
+
 export default mongoose.models.User || mongoose.model('User', UserSchema);
